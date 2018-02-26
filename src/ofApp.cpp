5f666d806a4d461a4bff6c20ab1a0e44dbb8dbca
Jools <jools@Joolss-MBP.eduroam.student>
2018-02-26 16:53:51 +0000

    a little cleaner

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,30 +1,18 @@
-/* Audio input from Maximilian to Wekinator
- Adapted from Maximilian example code by Rebecca Fiebrink, 2016
- 
- You can copy and paste this and use it as a starting example.
- 
- Sends OSC to Wekinator. The number of inputs depends on what you select in the GUI.
- 
- */
-
-
 #include "ofApp.h"
 #include "maximilian.h"/* include the lib */
 #include "time.h"
 
-
-
 //-------------------------------------------------------------
+
+// deconstructor here why?
 ofApp::~ofApp() {
     
 }
-
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     
     sender.setup(HOST, PORT);
-    /* some standard setup stuff*/
     
     ofEnableAlphaBlending();
     ofSetupScreen();
@@ -48,11 +36,9 @@ void ofApp::setup(){
     memset(lAudioIn, 0, initialBufferSize * sizeof(float));
     memset(rAudioIn, 0, initialBufferSize * sizeof(float));
     
-    
     fftSize = 1024;
     mfft.setup(fftSize, 512, 256);
     ifft.setup(fftSize, 512, 256);
-    
     
     nAverages = 12;
     oct.setup(sampleRate, fftSize/2, nAverages);
@@ -64,20 +50,12 @@ void ofApp::setup(){
     ofxMaxiSettings::setup(sampleRate, 2, initialBufferSize);
     ofSoundStreamSetup(2,2, this, sampleRate, initialBufferSize, 4);/* Call this last ! */
     
-    
     //GUI STUFF
     gui.setup(); // most of the time you don't need a name
-    //gui.add(fftToggle.setup("FFT bin magnitudes (pitch/timbre/volume) (512 inputs)", true));
     gui.add(mfccToggle.setup("MFCCs (timbre/vocal) (13 inputs)", true));
-    //gui.add(chromagramToggle.setup("Const-Q analyser (12 bands/oct) (104 inputs)", true));
-    //gui.add(peakFrequencyToggle.setup("Peak frequency (pitch) (1 input)", true));
-    //gui.add(centroidToggle.setup("Spectral centroid (timbre) (1 input)", true));
     gui.add(rmsToggle.setup("RMS (volume) (1 input)", true));
     
     bHide = true;
-    
-    //myFont.loadFont("arial.ttf", 18); //requires this to be in bin/data/
-    //myFont2.loadFont("arial.ttf", 12); //requires this to be in bin/data/
     
     /* currently the below sound stream inputs just one channel. Perhaps I need to set up the whole block of code for each channel? */
     
@@ -90,9 +68,6 @@ void ofApp::setup(){
     // what exactly is "this" reffering to here?
     audioStream.setInput(this);
     
-    // third value in this method is the device id to be used (in this case echo )
-    // ofSoundStreamSetup(2, 1, sampleRate, initialBufferSize, 4);/* Call this last ! */
-    
     // why has adding "this" to this line resulted in functionality?
     audioStream.setup(this, 0, 1, sampleRate, initialBufferSize, 4);
     
@@ -100,62 +75,17 @@ void ofApp::setup(){
     audioStream.getNumInputChannels();
     std::cout << "no of input channels = " << audioStream.getNumInputChannels() << endl;
     
-    
     ofSetVerticalSync(true);
-    
-    
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     
-//    ofxOscMessage m;
-//    m.setAddress("/wek/inputs");
-    
-//    if (fftToggle) {
-//        // cout << "FFT" << endl;
-//        //m.setAddress("/fft");
-//        for (int i = 0; i < fftSize/2; i++) {
-//            m.addFloatArg(mfft.magnitudes[i]);
-//
-//        }
-//        // sender.sendMessage(m);
-//    }
-//
-    
-//    if (peakFrequencyToggle) {
-//        // cout << "peak" << endl;
-//
-//        //ofxOscMessage m;
-//        // m.setAddress("/peakFrequency");
-//        m.addFloatArg(peakFreq);
-//        // sender.sendMessage(m);
-//    }
-//
-//    if (centroidToggle) {
-//        // cout << "centr" << endl;
-//
-//        //// ofxOscMessage m;
-//        // m.setAddress("/centroid");
-//        m.addFloatArg(centroid);
-//        // sender.sendMessage(m);
-//    }
-    
     ofxOscMessage m;
     m.setAddress("/wek/inputs");
-//    for (int i = 0; i < 13; i++) {
-//        m.addFloatArg(0);
-//    }
-    
-/* currently set up with threshold so that if rms goes above threshold it sends out osc message. In this way rms is now a bolean switch */
     
     if (rmsToggle) {
-        
         if (RMS > 2){
-        
-        cout << "rms" << endl;
-        
         ofxOscMessage m;
         m.setAddress("/wek/inputs");
         for (int i = 0; i < 13; i++) {
@@ -168,8 +98,6 @@ void ofApp::update(){
     
     else if
         (mfccToggle) {
-        cout << "mfcc" << endl;
-        
         ofxOscMessage m;
         m.setAddress("/wek/inputs");
         for (int i = 0; i < 13; i++) {
@@ -177,34 +105,27 @@ void ofApp::update(){
             cout << "mfccs = " << mfccs[i] << endl;
         }
         sender.sendMessage(m);
-        
     }
-//}
-    
-    // perhaps neccesary to add an else statement here to set it to mfcc by default? This way we keep it listening to stuff and only activating if it hears one or the other as when necessary
-    
 }
-
-
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
     float horizWidth = 500.;
     float horizOffset = 100;
-    float fftTop = 250;
+    //float fftTop = 250;
     float mfccTop = 350;
-    float chromagramTop = 450;
+    //float chromagramTop = 450;
     
     ofSetColor(255, 0, 0,255);
     
-    //FFT magnitudes:
+//    //FFT magnitudes:
     float xinc = horizWidth / fftSize * 2.0;
-    for(int i=0; i < fftSize / 2; i++) {
-        float height = mfft.magnitudes[i] * 100;
-        ofRect(horizOffset + (i*xinc),250 - height,2, height);
-    }
-    //myFont.drawString("FFT:", 30, 250);
+//    for(int i=0; i < fftSize / 2; i++) {
+//        float height = mfft.magnitudes[i] * 100;
+//        ofRect(horizOffset + (i*xinc),250 - height,2, height);
+//    }
+//    //myFont.drawString("FFT:", 30, 250);
     
     
     //MFCCs:
@@ -215,69 +136,48 @@ void ofApp::draw(){
         ofRect(horizOffset + (i*xinc),mfccTop - height,40, height);
         //        cout << mfccs[i] << ",";
     }
-    //myFont.drawString("MFCCs:", 12, mfccTop);
-    
-    
-    //Const-Q:
-    ofSetColor(255, 0, 255,200);
-    xinc = horizWidth / oct.nAverages;
-    for(int i=0; i < oct.nAverages; i++) {
-        float height = oct.averages[i] / 20.0 * 100;
-        ofRect(horizOffset + (i*xinc),chromagramTop - height,2, height);
-    }
-    //myFont.drawString("ConstQ:", 12, chromagramTop);
-    
     
     ofSetColor(255, 255, 255,255);
-    
-    char peakString[255]; // an array of chars
-    sprintf(peakString, "Peak Frequency: %.2f", peakFreq);
-    //myFont.drawString(peakString, 12, chromagramTop + 50);
-    
-    char centroidString[255]; // an array of chars
-    sprintf(centroidString, "Spectral Centroid: %f", centroid);
-    //myFont.drawString(centroidString, 12, chromagramTop + 80);
+//
+//    char peakString[255]; // an array of chars
+//    sprintf(peakString, "Peak Frequency: %.2f", peakFreq);
+//    //myFont.drawString(peakString, 12, chromagramTop + 50);
+//
+//    char centroidString[255]; // an array of chars
+//    sprintf(centroidString, "Spectral Centroid: %f", centroid);
+//    //myFont.drawString(centroidString, 12, chromagramTop + 80);
     
     char rmsString[255]; // an array of chars
     sprintf(rmsString, "RMS: %.2f", RMS);
     //myFont.drawString(rmsString, 12, chromagramTop + 110);
     
     int numInputs = 0;
-    if (fftToggle) {
-        numInputs += fftSize/2;
-    }
+
     if (mfccToggle) {
         numInputs += 13;
+        std::cout << "MFCC inputs = " << numInputs << endl;
     }
-    if (chromagramToggle) {
-        numInputs += 104;
-    }
-    if (peakFrequencyToggle) {
-        numInputs++;
-    }
-    if (centroidToggle) {
-        numInputs++;
-    }
+    
+    // rms num inputs seems to be 1 + mfcc inputs. Why?
     if (rmsToggle) {
         numInputs++;
+        std::cout << "rms inputs = " << numInputs << endl;
     }
     
     char numInputsString[255]; // an array of chars
     sprintf(numInputsString, "Sending %d inputs total", numInputs);
     //myFont.drawString(numInputsString, 450, 100);
-    
-    
-    
+
     if( bHide ){
         gui.draw();
     }
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::audioRequested     (float * output, int bufferSize, int nChannels){
     for (int i = 0; i < bufferSize; i++){
         wave = lAudioIn[i];
+        //std::cout << "audio = " << wave << endl;
         if (mfft.process(wave)) {
             
             mfft.magsToDB();
@@ -296,8 +196,6 @@ void ofApp::audioRequested     (float * output, int bufferSize, int nChannels){
             }
             centroid = sum / (fftSize / 2);
             peakFreq = (float)maxBin/fftSize * 44100;
-            
-            
             mfcc.mfcc(mfft.magnitudes, mfccs);
         }
         
@@ -305,9 +203,6 @@ void ofApp::audioRequested     (float * output, int bufferSize, int nChannels){
         rAudioOut[i] = 0;
         
     }
-    
-    
-    
 }
 
 //--------------------------------------------------------------
@@ -343,7 +238,6 @@ void ofApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-    
     
     
 }
