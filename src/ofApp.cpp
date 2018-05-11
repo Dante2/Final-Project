21433aada2935196b34e5585327f6d3c2190da69
Jools <jools@Joolss-MBP.eduroam.student>
2018-05-11 14:58:13 +0100

    synth hooked up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -200,8 +200,6 @@ void ofApp::update(){
             quaternion = p.getArgAsFloat(0);
             cout << "quaternion = " << quaternion << endl;
         }
-        
-        
     }
 }
 
@@ -269,44 +267,7 @@ void ofApp::draw(){
     if( bHide ){
         gui.draw();
     }
-
-//    ofSetColor(255);
-//    for ( int i=0; i<myo.getDevices().size(); i++ ) {
-//        stringstream s;
-//        s << "id: " << myo.getDevices()[i]->getId() << endl;
-//        s << "which: " << myo.getDevices()[i]->getWhichArm() << endl;
-//        s << "pose: " << myo.getDevices()[i]->getPose() << endl;
-//        s << "accel:          ";
-//        s << myo.getDevices()[i]->getAccel().x << ",";
-//        s << myo.getDevices()[i]->getAccel().y << ",";
-//        s << myo.getDevices()[i]->getAccel().z << endl;
-//        s << "gyro:           ";
-//        s << myo.getDevices()[i]->getGyro().x << ",";
-//        s << myo.getDevices()[i]->getGyro().y << ",";
-//        s << myo.getDevices()[i]->getGyro().z << endl;
-//        s << "quaternion:     ";
-//        s << myo.getDevices()[i]->getQuaternion().x() << ",";
-//        s << myo.getDevices()[i]->getQuaternion().y() << ",";
-//        s << myo.getDevices()[i]->getQuaternion().z() << ",";
-//        s << myo.getDevices()[i]->getQuaternion().w() << endl;
-//        s << "roll/pitch/yaw: ";
-//        s << myo.getDevices()[i]->getRoll() << ",";
-//        s << myo.getDevices()[i]->getPitch() << ",";
-//        s << myo.getDevices()[i]->getYaw() << endl;
-//        s << "raw data:       ";
-//        for ( int j=0; j<8; j++ ) {
-//            s << myo.getDevices()[i]->getEmgSamples()[j];
-//            s << ",";
-//        }
-//        s << endl;
-//        ofSetColor(255);
-//        ofDrawBitmapString(s.str(), 0,12 + i*100);
-//    }
 }
-
-//void ofApp::exit(){
-//    myo.stop();
-//}
 
 //--------------------------------------------------------------
 void ofApp::audioRequested     (float * output, int bufferSize, int nChannels){
